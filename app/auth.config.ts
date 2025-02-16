bccdeaaf536652d250b8ddfaa75a8e4d04e85525
huavcjj <huavcjj0217@icloud.com>
2025-02-16 16:34:13 +0900

    Changes

---
+++ app/auth.config.ts
@@ -0,0 +1,20 @@
+import { NextAuthConfig } from "next-auth";
+
+export const authConfig = {
+  pages: {
+    signIn: "/login",
+  },
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user;
+      const isOndashboard = nextUrl.pathname.startsWith("/dashboard");
+      if (isOndashboard) {
+        return isLoggedIn;
+      } else if (isLoggedIn) {
+        return Response.redirect(new URL("/dashboard", nextUrl));
+      }
+      return true;
+    },
+  },
+  providers: [],
+} satisfies NextAuthConfig;
